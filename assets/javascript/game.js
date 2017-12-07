55193d8a9522a03552b85c5c854b5aaa9996671b
Dave Funk <Dave@Daves-MacBook-Pro.local>
2017-12-06 21:26:21 -0800

    started making variables and put up a bootstrap-template I will customize later.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,12 @@
-var wordList = [,]
+var wordList = ["Baltimore Ravens", "Arizona Cardinals", "Atlanta Falcons", "Bufalo Bills",
+"Carolina Panthers", "Chicago Bears", "Cincinnati Bengals", "Cleveland Browns", "Dallas Cowboys",
+"Denver Broncos", "Detroit Lions", "Green Bay Packers", "Houston Texans", "Indianapolis Colts",
+"Jacksonville Jaguars", "Kansas City Cheifs", "Los Angeles Chargers", "Los Angeles Rams",
+"Miami Dolphins", "Minnesota Vikings", "New England Patriots", "New Orleans Saints",
+"New York Giants", "New York Jets", "Oakland Raiders", "Philadelphia Eagles", "Pittsburgh Steelers",
+"San Fransisco Forty Niners", "Seattle Seahawks", "Tampa Bay Buccaneers", "Tennessee Titans",
+"Washington Redskins"];
+var wins = [""]
+var lettersGuessed = [""]
+var guessesRemaining = [""]
+var chosenWord = [""]
