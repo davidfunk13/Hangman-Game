76ed465c198883793a03055d2cd6ac2eb6f8a8c6
Dave <davefunk135@gmail.com>
2017-12-07 17:15:09 -0800

    more trial and error

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,29 @@
 var wordList = ["Ravens", "Cardinals", "Falcons", "Bills",
-"Panthers", "Bears", "Bengals", "Browns", "Cowboys",
-"Broncos", "Lions", "Packers", "Texans", "Colts",
-"Jaguars", "Cheifs", "Chargers", "Rams",
-"Dolphins", "Vikings", "Patriots", "Saints",
-"Giants", "Jets", "Raiders", "Eagles", "Steelers",
-"Forty Niners", "Seahawks", "Buccaneers", "Titans",
-"Redskins"];
-var chosenWord = [];
+    "Panthers", "Bears", "Bengals", "Browns", "Cowboys",
+    "Broncos", "Lions", "Packers", "Texans", "Colts",
+    "Jaguars", "Cheifs", "Chargers", "Rams",
+    "Dolphins", "Vikings", "Patriots", "Saints",
+    "Giants", "Jets", "Raiders", "Eagles", "Steelers",
+    "Forty Niners", "Seahawks", "Buccaneers", "Titans",
+    "Redskins"];
+var wordBlanks = [];
+var guess = keyListen();
+
+    // Key listener:
+    function keyListen() {
+        document.addEventListener("keypress", letterPressed);
+        function letterPressed(event) {
+            var letter = String.fromCharCode(event.keyCode);
+            console.log(letter)
+        }
+    }
+
+
+// Start game function
+// function startGame() {
 var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
-
-
-
-// Key listener:
-// document.addEventListener("keypress", letterPressed);
-// function letterPressed(event) {
-//     var letter = String.fromCharCode(event.keyCode);
-//   console.log(letter)
-// }
-
-    for (var i = 0; i < randomWord.length; i++) {
-        chosenWord[i] = "_";
-        console.log(chosenWord,randomWord);
-        document.getElementById("spaces").innerHTML = randomWord;
-
-    };
+for (var i = 0; i < randomWord.length; i++) {
+    wordBlanks[i] = "_";
+    // console.log(wordBlanks,randomWord);
+    document.getElementById("spaces").innerHTML = wordBlanks.join(" ");
+};
