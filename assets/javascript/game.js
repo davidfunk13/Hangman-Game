858e4dff126d263584a22fd9a8a88908a474c9ff
Dave Funk <Dave@Daves-MacBook-Pro.local>
2017-12-08 16:02:55 -0800

    have added a function to split the string of the random word chosen into an array.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,24 +5,31 @@ var wordList = ["Ravens", "Cardinals", "Falcons", "Bills",
     "Dolphins", "Vikings", "Patriots", "Saints",
     "Giants", "Jets", "Raiders", "Eagles", "Steelers",
     "Forty Niners", "Seahawks", "Buccaneers", "Titans",
-    "Redskins"];
+    "Redskins"
+];
 var wordBlanks = [];
 var guessedLetters = [];
-// var guess = prompt("One letter guess")
+var splitRandomWord = [];
 
-    // Key listener:
-        document.addEventListener("keypress", letterPressed);
-        function letterPressed(event) {
-            var letter = String.fromCharCode(event.keyCode);
-            guessedLetters.push(letter);
-            document.getElementById("lettersGuessed").innerHTML = guessedLetters;
-            console.log(guessedLetters)
-        }
-// var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
-// for (var i = 0; i < randomWord.length; i++) {
-//     wordBlanks[i] = "_";
-//     // console.log(wordBlanks,randomWord);
-//     document.getElementById("spaces").innerHTML = wordBlanks.join(" ");
-// };
-// guess functionality
-// if (guess === )
+var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+for (var i = 0; i < randomWord.length; i++) {
+    wordBlanks[i] = "_";
+    console.log(wordBlanks, randomWord);
+    document.getElementById("spaces").innerHTML = wordBlanks.join(" ");
+};
+splitRandomWord.push(randomWord);
+console.log(splitRandomWord);
+
+function splitString(randomWord) {
+    var splitRandomWord = randomWord.split("");
+    console.log(splitRandomWord);
+}
+
+// Key listener:
+document.addEventListener("keypress", letterPressed);
+function letterPressed(event) {
+    var letter = String.fromCharCode(event.keyCode);
+    guessedLetters.push(letter);
+    document.getElementById("lettersGuessed").innerHTML = guessedLetters;
+    console.log(guessedLetters)
+}
