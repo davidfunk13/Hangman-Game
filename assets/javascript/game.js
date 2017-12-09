b04def1d1d08e8616015f85c7f28e3564744b660
Dave Funk <Dave@Daves-MacBook-Pro.local>
2017-12-08 18:48:07 -0800

    current state update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,47 @@
-var wordList = ["Ravens", "Cardinals", "Falcons", "Bills",
-    "Panthers", "Bears", "Bengals", "Browns", "Cowboys",
-    "Broncos", "Lions", "Packers", "Texans", "Colts",
-    "Jaguars", "Cheifs", "Chargers", "Rams",
-    "Dolphins", "Vikings", "Patriots", "Saints",
-    "Giants", "Jets", "Raiders", "Eagles", "Steelers",
-    "Forty Niners", "Seahawks", "Buccaneers", "Titans",
-    "Redskins"
-];
-var wordBlanks = [];
-var guessedLetters = [];
-var splitRandomWord = [];
-
+// Declarations
+var wordList = ["RAVENS", "CARDINALS", "FALCONS", "BILLS",
+"PANTHERS", "BEARS", "BENGALS", "BROWNS", "COWBOYS",
+"BRONCOS", "LIONS", "PACKERS", "TEXANS", "COLTS",
+"JAGUARS", "CHEIFS", "CHARGERS", "RAMS",
+"DOLPHINS", "VIKINGS", "PATRIOTS", "SAINTS",
+"GIANTS", "JETS", "RAIDERS", "EAGLES", "STEELERS",
+"FORTYNINERS", "SEAHAWKS", "BUCCANEERS", "TITANS",
+"REDSKINS"];
+// Picks a word at Random
 var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+// Array for conversion of randomWord to "_" strings
+var underscores = [];
+//Array for keylogged guesses
+var guessedLetters = [];
+//Splits chosen word into seperate letter strings
+var splitRandomWord = randomWord.split("");
+//a function to test it with
+function stringSplit() {
+console.log(splitRandomWord);
+}
+// This runs through the chosen randomWord and for each character in it returns and "_" in a new array
 for (var i = 0; i < randomWord.length; i++) {
-    wordBlanks[i] = "_";
-    console.log(wordBlanks, randomWord);
-    document.getElementById("spaces").innerHTML = wordBlanks.join(" ");
+underscores[i] = "_";
+console.log(underscores, randomWord);
+document.getElementById("spaces").innerHTML = underscores.join(" ");
 };
-// splitRandomWord.push(randomWord);
-// console.log(splitRandomWord);
+stringSplit();
 
-function splitString(randomWord) {
-    var splitRandomWord = randomWord.split("");
-    console.log(splitRandomWord);
-}
-
-// Key listener:
+// Detects user's keystrokes, logs them to an array "lettersguessed" displayed on the htmlDOC.
 document.addEventListener("keypress", letterPressed);
-function letterPressed(event) {
-    var letter = String.fromCharCode(event.keyCode);
-    guessedLetters.push(letter);
-    document.getElementById("lettersGuessed").innerHTML = guessedLetters;
-    console.log(guessedLetters)
+function letterPressed() {
+var letter = String.fromCharCode(event.keyCode);
+guessedLetters.push(letter);
+document.getElementById("lettersGuessed").innerHTML = guessedLetters;
+console.log(guessedLetters);
+};
+
+//Who even knows
+function check() {
+for (var j = 0; j < splitRandomWord.length; j++) {
+    if (guessedLetters[j] === splitRandomWord.indexOf(j)) {
+        console.log("works?");
+    }
 }
+}
+// Guessing framework
