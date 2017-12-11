a4c440907efb4a161ebb09015d820a7df1595a4d
Dave Funk <Dave@Daves-MacBook-Pro.local>
2017-12-10 22:58:06 -0800

    got everything working except win conditions. I feel i have a solid understanding of for loops, control flow, variables and functions. I was not able to figure out the use of data-letter, getting the values of the assigned spans into strings, then referencing them for win conditions. I am very dissapointed that I was not able to finish by the time allotted. I have put many, many, many hours into this. However, upon 'completion' of the assignment, I did find through rebuilding the app to try to further understand, I was able to reproduce it on my own fairly quickly, but still didnt get any further insight into the htmlelement.dataset property. I feel like I'm literally one step away from it working, and my code could be a lot cleaner as i got dizzy and disorganized trying to solve itin the end. Overall this was an amazing learning experience and I look forward to next weeks project.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,12 @@
 //Declarations
 var wordList = ["RAVENS", "CARDINALS", "FALCONS", "BILLS",
-    "PANTHERS", "BEARS", "BENGALS", "BROWNS", "COWBOYS",
-    "BRONCOS", "LIONS", "PACKERS", "TEXANS", "COLTS",
-    "JAGUARS", "CHEIFS", "CHARGERS", "RAMS",
-    "DOLPHINS", "VIKINGS", "PATRIOTS", "SAINTS",
-    "GIANTS", "JETS", "RAIDERS", "EAGLES", "STEELERS",
-    "FORTYNINERS", "SEAHAWKS", "BUCCANEERS", "TITANS",
-    "REDSKINS"
+"PANTHERS", "BEARS", "BENGALS", "BROWNS", "COWBOYS",
+"BRONCOS", "LIONS", "PACKERS", "TEXANS", "COLTS",
+"JAGUARS", "CHEIFS", "CHARGERS", "RAMS",
+"DOLPHINS", "VIKINGS", "PATRIOTS", "SAINTS",
+"GIANTS", "JETS", "RAIDERS", "EAGLES", "STEELERS",
+"FORTYNINERS", "SEAHAWKS", "BUCCANEERS", "TITANS",
+"REDSKINS"
 ];
 
 // Picks a word at Random
@@ -19,10 +19,10 @@ var underscores = [];
 var guessedLetters = [];
 
 //Splits chosen word into seperate letter strings
-var splitRandomWord = randomWord.split(""); 
+var splitRandomWord = randomWord.split("");
 
 //Correct guesses array 
-var correctGuesses = [];                  
+var correctGuesses = [];
 
 //Guesses remaining
 var guessesRemaining = 8;
@@ -33,46 +33,49 @@ document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
 // This runs through the chosen randomWord and for each character in it returns and "_" in a new array
 const spacesDiv = document.getElementById('spaces');
 for (var i = 0; i < splitRandomWord.length; i++) {
-    var underscores = document.createElement('span');
-    underscores.setAttribute('data-letter', splitRandomWord[i]);
-    underscores.textContent = '_';
-    underscores.style.marginRight = '1ch';
-    spacesDiv.appendChild(underscores);
-    if (guessedLetters === splitRandomWord) {
-        alert("YOU WIN!");
-    }
+var underscores = document.createElement('span');
+underscores.setAttribute('data-letter', splitRandomWord[i]);
+underscores.textContent = '_';
+underscores.style.marginRight = '1ch';
+spacesDiv.appendChild(underscores);
 }
 console.log(splitRandomWord);
 
 // Detects user's keystrokes, logs them to an array "lettersguessed" displayed on the htmlDOC.
 document.addEventListener("keypress", letterPressed);
-var letters = document.querySelectorAll("span[data-letter]");                
+var dataLetters = document.querySelectorAll("span[data-letter]");
 
 // Keylogger + Input Conditional statements
 function letterPressed() {
-    letter = String.fromCharCode(event.keyCode).toUpperCase();
+letter = String.fromCharCode(event.keyCode).toUpperCase();
 
+if (splitRandomWord.includes(letter) === false) {
+    guessedLetters.push(letter);
+    console.log("there's no " + letter + " in this word.")
+    document.getElementById("lettersGuessed").innerHTML = guessedLetters;
+    // console.log(guessedLetters)
     if (splitRandomWord.includes(letter) === false) {
-        guessedLetters.push(letter);
-        console.log("there's no " + letter + " in this word.")
-        document.getElementById("lettersGuessed").innerHTML = guessedLetters;
-        console.log(guessedLetters)
-        if (splitRandomWord.includes(letter) === false) {
-            guessesRemaining--;
-            document.getElementById("guessesRemaining").innerHTML = guessesRemaining;            
-            if (guessesRemaining === 0) {
-                alert("You lose");
-            }
-        }
-    } else {
-        for (var j = 0; j < splitRandomWord.length; j++) {
-            if (letter === splitRandomWord[j]) {
-                console.log("correct! contains:" + letter);
-                correctGuesses.push(letter);
-                guessedLetters.push(letter);
-                document.getElementById("lettersGuessed").innerHTML = guessedLetters;               
-                letters[j].textContent = letter;   
-            }
+        guessesRemaining--;
+        document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
+        if (guessesRemaining === 0) {
+            alert("You lose");
         }
     }
+} else {
+    for (var j = 0; j < splitRandomWord.length; j++) {
+        if (letter === splitRandomWord[j]) {
+            console.log("correct! contains:" + letter);
+            correctGuesses.push(letter);
+            guessedLetters.push(letter);
+            document.getElementById("lettersGuessed").innerHTML = guessedLetters;
+            dataLetters[j].textContent = letter;
+        }
+    }
+    for (var k = 0; k < splitRandomWord.length; k++) {
+
+        if (dataLetters[k].textContent === splitRandomWord) {
+            console.log("you win");
+        }
+    }
+}
 };
